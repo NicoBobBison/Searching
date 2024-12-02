2503ac3b8c38f8b8faf22d9855568be5c30cc5e4
NicoBobBison <47168714+NicoBobBison@users.noreply.github.com>
2024-12-02 04:01:47 -0500

    Command line arguments

--- main.py
+++ main.py
@@ -1,21 +1,25 @@
 from matplotlib import pyplot
 import networkx as nx
+import argparse
 from src.search import *
 
-path = "./graphs/monroe.txt"
-start = "i406"
-end = "i5000"
+parser = argparse.ArgumentParser(description="A program to test various search algorithms.")
+parser.add_argument("filepath", help="File path to raw graph data (.txt).")
+parser.add_argument("start", help="Name of starting intersection.")
+parser.add_argument("end", help="Name of ending intersection.")
+parser.add_argument("--map", help="Shows an image of the map and shortest path.", action="store_true")
+args = parser.parse_args()
 
 G = nx.Graph()
 intersections = 0
 roads = 0
-file = open(path, "r")
+file = open(args.filepath, "r")
 pos = {}
 for line in file:
     line = line.strip()
     list = line.split("\t")
     if list[0] == "i":
-        node_pos = (float(list[2]), float(list[3]))
+        node_pos = (float(list[3]), float(list[2]))
         pos[list[1]] = node_pos
         G.add_node(node_for_adding=list[1], id=list[1], pos=node_pos)
         intersections += 1
@@ -28,16 +32,17 @@ print(f"Found {intersections} intersections.")
 print(f"Found {roads} roads")
 
 s = Searcher(G, dijkstras)
-path = s.search(start, end)
-if path is None:
-    print("Could not find a path")
+path = s.search(args.start, args.end)
+if len(path) == 0:
+    print(f"Could not find a path between {args.start} and {args.end}")
 else:
     print(f"Found path {path}")
 path_edges = [(path[n], path[n+1]) for n in range(len(path) - 1)]
 
-nx.draw_networkx_edges(G, pos=pos)
-nx.draw_networkx_edges(G, pos=pos, edgelist=path_edges, width=1.5, edge_color="tab:red")
-nx.draw_networkx_nodes(G, pos=pos, nodelist=[start, end], node_size=10, node_color="tab:red")
-nx.draw_networkx_labels(G, pos=pos, labels={start:start, end:end}, font_color="tab:red")
+if args.map:
+    nx.draw_networkx_edges(G, pos=pos)
+    nx.draw_networkx_edges(G, pos=pos, edgelist=path_edges, width=1.5, edge_color="tab:red")
+    nx.draw_networkx_nodes(G, pos=pos, nodelist=[args.start, args.end], node_size=10, node_color="tab:red")
+    nx.draw_networkx_labels(G, pos=pos, labels={args.start:args.start, args.end:args.end}, font_color="tab:red")
 
-pyplot.show()
+    pyplot.show()
