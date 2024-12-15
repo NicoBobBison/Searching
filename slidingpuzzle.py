55f47ab4d922ff9e70fa4ad57a07b92a9288ea47
NicoBobBison <47168714+NicoBobBison@users.noreply.github.com>
2024-12-15 11:44:30 -0800

    Switched dicts to list, fixed solvable

--- slidingpuzzle.py
+++ slidingpuzzle.py
@@ -5,10 +5,9 @@ import copy
 import random
 
 class State:
-    def __init__(self, width, id_to_pos, pos_to_id) -> None:
+    def __init__(self, width, state) -> None:
         self.width = width
-        self.id_to_pos = id_to_pos
-        self.pos_to_id = pos_to_id
+        self.state = state
 
     def neighbors(self):
         def should_remove(i):
@@ -24,44 +23,32 @@ class State:
             return False
 
         neighbors = []
-        blank = self.id_to_pos["-"]
+        blank = self.state.index("-")
         # Possible directions to move the blank
         possible_dir = [blank + 1, blank - 1, blank + self.width, blank - self.width]
         possible_dir[:] = [i for i in possible_dir if not should_remove(i)]
         
         for p in possible_dir:
-            swapped = self.pos_to_id[p]
+            swapped = self.state[p]
             self.swap_id("-", swapped)
-            neighbors.append(State(self.width, copy.deepcopy(self.id_to_pos), copy.deepcopy(self.pos_to_id)))
+            neighbors.append(State(self.width, copy.deepcopy(self.state)))
             self.swap_id("-", swapped)
         return neighbors
         
     def swap_id(self, id1, id2):
-        pos1 = self.id_to_pos[id1]
-        pos2 = self.id_to_pos[id2]
+        self.swap_index(self.state.index(id1), self.state.index(id2))
 
-        self.id_to_pos.pop(id1)
-        self.id_to_pos.pop(id2)
-        self.pos_to_id.pop(pos1)
-        self.pos_to_id.pop(pos2)
-
-        self.id_to_pos[id1] = pos2
-        self.id_to_pos[id2] = pos1
-        self.pos_to_id[pos1] = id2
-        self.pos_to_id[pos2] = id1
-
-    def load_config(self, config):
-        for i in range(len(config)):
-            self.id_to_pos[config[i]] = i
-            self.pos_to_id[i] = config[i]
+    def swap_index(self, index1, index2):
+        temp = self.state[index1]
+        self.state[index1] = self.state[index2]
+        self.state[index2] = temp
     
     def __str__(self) -> str:
-        if len(self.id_to_pos) == 0:
+        if len(self.state) == 0:
             return ""
         str = ""
-        keys = sorted(self.pos_to_id.keys())
-        for i in range(len(keys)):
-            str += f"{self.pos_to_id[keys[i]]} "
+        for i in range(len(self.state)):
+            str += f"{self.state[i]} "
             if i % self.width == self.width - 1:
                 str += "\n"
         return str
@@ -72,7 +59,7 @@ class State:
     def __eq__(self, value: object) -> bool:
         if value is None:
             return False
-        return self.id_to_pos == value.id_to_pos
+        return self.state == value.state
     
     def __hash__(self) -> int:
         return hash(self.__str__())
@@ -80,10 +67,10 @@ class State:
 # Counts the number of wrong cells
 def wrong_count(P: Problem, node: State):
     count = 0
-    for i in range(len(node.pos_to_id)):
-        if node.pos_to_id[i] == "-":
+    for i in range(len(node.state)):
+        if node.state[i] == "-":
             continue
-        if i != node.pos_to_id[i]:
+        if i != node.state[i]:
             count += 1
     return count
 
@@ -91,13 +78,13 @@ def wrong_count(P: Problem, node: State):
 # Much better heuristic, used by default
 def manhattan_to_correct(P: Problem, node: State):
     count = 0
-    for i in range(len(node.pos_to_id)):
-        if node.pos_to_id[i] == "-":
+    for i in range(len(node.state)):
+        if node.state[i] == "-":
             continue
         x_desired = i % node.width
         y_desired = i // node.width
-        x_actual = int(node.pos_to_id[i]) % node.width
-        y_actual = int(node.pos_to_id[i]) // node.width
+        x_actual = int(node.state[i]) % node.width
+        y_actual = int(node.state[i]) // node.width
         count += abs(x_desired - x_actual) + abs(y_desired - y_actual)
     return count
 
@@ -111,12 +98,20 @@ def expand(P: Problem, node):
 
 def solvable(s: State):
     count = 0
-    for i in range(len(s.pos_to_id)):
-        if s.pos_to_id[i] == "-" or (i + 1 < len(s.pos_to_id) and s.pos_to_id[i+1] == "-"):
-            continue
-        if i + 1 < len(s.pos_to_id) and s.pos_to_id[i] > s.pos_to_id[i+1]:
-            count += 1
-    return count % 2 == 0
+    for i in range(len(s.state)):
+        for j in range(i + 1, len(s.state)):
+            if s.state[i] == "-" or s.state[j] == "-":
+                continue
+            if int(s.state[i]) > int(s.state[j]):
+                count += 1
+
+    if s.width % 2 == 1:
+        return count % 2 == 0
+    else:
+        blank = s.state.index("-")
+        row = blank // s.width
+        row_from_bottom = (s.width - row) + 1
+        return (count % 2 == 0 and row_from_bottom % 2 == 1) or (count % 2 == 1 and row_from_bottom % 2 == 0)
 
 parser = argparse.ArgumentParser(description="A program to test various search algorithms for 8 and 16 puzzles.")
 parser.add_argument("type", choices=["8", "16"], help="The size of the puzzle.")
@@ -126,19 +121,19 @@ args = parser.parse_args()
 input = input(f"Enter {args.type} unique integers from 1 to {"8" if args.type == "8" else "15"} and a blank (\"-\") separated by spaces, or enter \"random\" for a random puzzle:\n")
 
 width = 3 if args.type == "8" else 4
-s = State(width, {}, {})
+s = State(width, [])
 
 if input == "random":
     config = ["-"]
     for i in range(1, width ** 2):
         config.append(str(i))
     random.shuffle(config)
-    s.load_config(config)
+    s.state = config
     while not solvable(s):
         random.shuffle(config)
-        s.load_config(config)
+        s.state = config
 else:
-    s.load_config(input.split(" "))
+    s.state = input.split(" ")
 
 print(s)
 
@@ -146,11 +141,11 @@ if not solvable(s):
     print("This puzzle is not solvable!")
     exit()
 
-final = State(width, {}, {})
+final = State(width, [])
 if width == 3:
-    final.load_config(["-", "1", "2", "3", "4", "5", "6", "7", "8"])
+    final.state = ["-", "1", "2", "3", "4", "5", "6", "7", "8"]
 else:
-    final.load_config(["-", "1", "2", "3", "4", "5", "6", "7", "8", "9", "10", "11", "12", "13", "14", "15"])
+    final.state = ["-", "1", "2", "3", "4", "5", "6", "7", "8", "9", "10", "11", "12", "13", "14", "15"]
 
 G = nx.Graph()
 G.add_node(s)
